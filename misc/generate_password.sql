476b3a4814a2743cab900738e927fad52ac7ec35
NikolayS <github2@samokhvalov.com>
2017-05-10 11:00:28 -0700

    generate random password

---
+++ misc/generate_password.sql
@@ -0,0 +1,13 @@
+with init(len, arr) as (
+  select 16, string_to_array('23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ&#%@', null)
+), arrlen(l) as (
+  select count(*)
+  from (select unnest(arr) from init) _
+), indexes(i) as (
+  select 1 + int4(random() * (l - 1))
+  from arrlen, (select generate_series(1, len) from init) _
+)
+select array_to_string(array_agg(arr[i]), '') as password
+from init, indexes
+;
+
