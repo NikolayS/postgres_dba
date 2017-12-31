ea22148aba986af8e12e5c0f8d2183df9c707595
Nikolay Samokhvalov <github2@samokhvalov.com>
2017-12-31 12:15:00 -0800

    fix 1_basic: get info for current database

--- sql/1_basic.sql
+++ sql/1_basic.sql
@@ -1,6 +1,6 @@
 --Basic Node Information (master/replica, lag, DB size, tmp files)
 with data as (
-  select * from pg_stat_database where datname not in ('postgres', 'template0', 'template1') order by tup_returned desc limit 1
+  select * from pg_stat_database where datname = current_database()
 )
 select 'Database Name' as metric, datname as value from data
 union all
