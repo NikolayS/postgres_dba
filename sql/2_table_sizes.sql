ae5002ca3ba00d55d5b136e956a2d5a899191fd5
Nikolay Samokhvalov <github2@samokhvalov.com>
2017-12-31 19:31:00 -0800

    better sizes report

--- sql/2_table_sizes.sql
+++ sql/2_table_sizes.sql
@@ -1,18 +1,31 @@
 --General Table Size Information
 
-SELECT *, pg_size_pretty(total_bytes) AS total
-    , pg_size_pretty(index_bytes) AS INDEX
-    , pg_size_pretty(toast_bytes) AS toast
-    , pg_size_pretty(table_bytes) AS TABLE
-  FROM (
-  SELECT *, total_bytes-index_bytes-COALESCE(toast_bytes,0) AS table_bytes FROM (
-      SELECT c.oid,nspname AS table_schema, relname AS TABLE_NAME
-              , c.reltuples AS row_estimate
-              , pg_total_relation_size(c.oid) AS total_bytes
-              , pg_indexes_size(c.oid) AS index_bytes
-              , pg_total_relation_size(reltoastrelid) AS toast_bytes
-          FROM pg_class c
-          LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
-          WHERE relkind = 'r'
-  ) a
-) a order by total_bytes desc nulls last;
+with data as (
+  select
+    c.oid,
+    nspname as table_schema,
+    relname as table_name,
+    c.reltuples as row_estimate,
+    pg_total_relation_size(c.oid) as total_bytes,
+    pg_indexes_size(c.oid) as index_bytes,
+    pg_total_relation_size(reltoastrelid) as toast_bytes,
+    pg_total_relation_size(c.oid) - pg_indexes_size(c.oid) - coalesce(pg_total_relation_size(reltoastrelid), 0) as table_bytes
+  from pg_class c
+  left join pg_namespace n on n.oid = c.relnamespace
+  where relkind = 'r'
+)
+select
+  table_schema,
+  table_name,
+  row_estimate,
+  pg_size_pretty(total_bytes) || ' (' || round(100 * total_bytes::numeric / sum(total_bytes) over (), 2)::text || '%)' as "total (% of all)",
+  pg_size_pretty(table_bytes) || ' (' || round(100 * table_bytes::numeric / sum(table_bytes) over (), 2)::text || '%)' as "table (% of all tables)",
+  pg_size_pretty(index_bytes) || ' (' || round(100 * index_bytes::numeric / sum(index_bytes) over (), 2)::text || '%)' as "index (% of all indexes)",
+  pg_size_pretty(toast_bytes) || ' (' || round(100 * toast_bytes::numeric / sum(toast_bytes) over (), 2)::text || '%)' as "toast (% of all toast data)",
+  total_bytes,
+  table_bytes,
+  index_bytes,
+  toast_bytes
+  oid
+from data
+order by total_bytes desc nulls last;
