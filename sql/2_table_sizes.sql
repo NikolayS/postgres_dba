3e95496a90f75cc634b745ec08c4c0e1928921f7
Nikolay Samokhvalov <github2@samokhvalov.com>
2018-01-01 10:20:19 -0800

    2: human-redable rows count

--- sql/2_table_sizes.sql
+++ sql/2_table_sizes.sql
@@ -17,11 +17,18 @@ with data as (
 select
   table_schema,
   table_name,
-  row_estimate,
+  '~' || case
+    when row_estimate > 10^12 then round(row_estimate::numeric / 10^12::numeric, 0)::text || 'T'
+    when row_estimate > 10^9 then round(row_estimate::numeric / 10^9::numeric, 0)::text || 'B'
+    when row_estimate > 10^6 then round(row_estimate::numeric / 10^6::numeric, 0)::text || 'M'
+    when row_estimate > 10^3 then round(row_estimate::numeric / 10^3::numeric, 0)::text || 'k'
+    else row_estimate::text
+  end as rows,
   pg_size_pretty(total_bytes) || ' (' || round(100 * total_bytes::numeric / sum(total_bytes) over (), 2)::text || '%)' as "total (% of all)",
   pg_size_pretty(table_bytes) || ' (' || round(100 * table_bytes::numeric / sum(table_bytes) over (), 2)::text || '%)' as "table (% of all tables)",
   pg_size_pretty(index_bytes) || ' (' || round(100 * index_bytes::numeric / sum(index_bytes) over (), 2)::text || '%)' as "index (% of all indexes)",
   pg_size_pretty(toast_bytes) || ' (' || round(100 * toast_bytes::numeric / sum(toast_bytes) over (), 2)::text || '%)' as "toast (% of all toast data)",
+  row_estimate,
   total_bytes,
   table_bytes,
   index_bytes,
