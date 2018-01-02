b8b0583744dfe55570b2e9c56480ee6818d3455d
Nikolay Samokhvalov <github2@samokhvalov.com>
2018-01-01 20:41:46 -0800

    2: fix % output (since TOTAL is also 100%)

--- sql/2_table_sizes.sql
+++ sql/2_table_sizes.sql
@@ -38,10 +38,10 @@ select
     when row_estimate > 10^3 then round(row_estimate::numeric / 10^3::numeric, 0)::text || 'k'
     else row_estimate::text
   end as rows,
-  pg_size_pretty(total_bytes) || ' (' || round(100 * total_bytes::numeric / sum(total_bytes) over (), 2)::text || '%)' as "total (% of all)",
-  pg_size_pretty(table_bytes) || ' (' || round(100 * table_bytes::numeric / sum(table_bytes) over (), 2)::text || '%)' as "table (% of all tables)",
-  pg_size_pretty(index_bytes) || ' (' || round(100 * index_bytes::numeric / sum(index_bytes) over (), 2)::text || '%)' as "index (% of all indexes)",
-  pg_size_pretty(toast_bytes) || ' (' || round(100 * toast_bytes::numeric / sum(toast_bytes) over (), 2)::text || '%)' as "toast (% of all toast data)"
+  pg_size_pretty(total_bytes) || ' (' || round(200 * total_bytes::numeric / sum(total_bytes) over (), 2)::text || '%)' as "total (% of all)",
+  pg_size_pretty(table_bytes) || ' (' || round(200 * table_bytes::numeric / sum(table_bytes) over (), 2)::text || '%)' as "table (% of all tables)",
+  pg_size_pretty(index_bytes) || ' (' || round(200 * index_bytes::numeric / sum(index_bytes) over (), 2)::text || '%)' as "index (% of all indexes)",
+  pg_size_pretty(toast_bytes) || ' (' || round(200 * toast_bytes::numeric / sum(toast_bytes) over (), 2)::text || '%)' as "toast (% of all toast data)"
   /*,
   row_estimate,
   total_bytes,
