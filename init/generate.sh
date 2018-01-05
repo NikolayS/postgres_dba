dbeeeba2db80b938c2079ea7649317119c0e3cc6
Nikolay Samokhvalov <github2@samokhvalov.com>
2018-01-05 13:13:49 -0800

    "wide" mode on/off switcher in menu

--- init/generate.sh
+++ init/generate.sh
@@ -14,6 +14,11 @@ do
   desc=$(head -n1 $f | sed -e 's/^--//g')
   printf "%s '%4s – %s'\n" "\\echo" "$prefix" "$desc" >> "$OUT"
 done
+echo "\\if :postgres_dba_xon" >> "$OUT"
+  printf "  %s '%4s – %s'\n" "\\echo" "x" "Turn Wide Mode OFF (currently ON): show less details, less columns" >> "$OUT"
+echo "\\else" >> "$OUT"
+  printf "  %s '%4s – %s'\n" "\\echo" "x" "Turn Wide Mode ON (currently OFF): show more details, more columns" >> "$OUT"
+echo "\\endif" >> "$OUT"
 printf "%s '%4s – %s'\n" "\\echo" "q" "Quit" >> "$OUT"
 echo "\\echo" >> "$OUT"
 echo "\\echo Type your choice and press <Enter>:" >> "$OUT"
@@ -26,11 +31,23 @@ do
   prefix=$(echo $f | sed -e 's/_.*$//g' -e 's/^.*\///g')
   echo ":d_stp::text = '$prefix' as d_step_is_$prefix," >> "$OUT"
 done
+echo ":d_stp::text = 'x' as d_step_is_x," >> "$OUT"
 echo ":d_stp::text = 'q' as d_step_is_q \\gset" >> "$OUT"
 
 echo "\\if :d_step_is_q" >> "$OUT"
 echo "  \\echo 'Bye!'" >> "$OUT"
 echo "  \\echo" >> "$OUT"
+echo "\\elif :d_step_is_x" >> "$OUT"
+  echo "\\if :postgres_dba_xon" >> "$OUT"
+    echo "set postgres_dba.wide = 'off';" >> "$OUT"
+    echo "  \\echo 'Wide mode turned OFF!'" >> "$OUT"
+    echo "  \\echo" >> "$OUT"
+  echo "\\else" >> "$OUT"
+    echo "set postgres_dba.wide = 'on';" >> "$OUT"
+    echo "  \\echo 'Wide mode turned ON!'" >> "$OUT"
+    echo "  \\echo" >> "$OUT"
+  echo "\\endif" >> "$OUT"
+  echo "  \\ir ./$OUT" >> "$OUT"
 for f in ./sql/*.sql
 do
   prefix=$(echo $f | sed -e 's/_.*$//g' -e 's/^.*\///g')
