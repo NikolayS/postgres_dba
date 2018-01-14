b5e8f9348261c5e36429a331e64e161f783e5d02
Nikolay Samokhvalov <github2@samokhvalov.com>
2018-01-13 21:29:48 -0800

    a1: char(N), reworked algorithm, more details in "wide" mode

--- sql/a1_alignment_padding.sql
+++ sql/a1_alignment_padding.sql
@@ -20,7 +20,7 @@ with recursive constants as (
       when 'i' then 4
       when 'd' then 8
       else null
-    end as shift,
+    end as _shift,
     case typalign
       when 's' then 1
       when 'i' then 2
@@ -28,7 +28,8 @@ with recursive constants as (
       when 'c' then
         case when typlen > 0 then typlen % chunk_size else 9 end
       else 9
-    end as alt_order_group
+    end as alt_order_group,
+    character_maximum_length
   from information_schema.columns
   join constants on true
   join pg_type on udt_name = typname
@@ -42,11 +43,16 @@ with recursive constants as (
     column_name,
     udt_name,
     typalign,
-    shift,
-    alt_order_group
+    _shift,
+    alt_order_group,
+    character_maximum_length
   from columns
 ), combined_columns as (
-  select * from columns union all select * from alt_columns
+  select *, coalesce(character_maximum_length, _shift) as shift
+  from columns
+  union all
+  select *, coalesce(character_maximum_length, _shift) as shift
+  from alt_columns
 ), analyze_alignment as (
   select
     is_orig,
@@ -57,8 +63,11 @@ with recursive constants as (
     '{}'::text[] as padded_columns,
     '{}'::int[] as pads,
     (select max(ordinal_position) from columns c where c.table_name = _.table_name and c.table_schema = _.table_schema) as col_cnt,
-    array_agg(_.column_name::text) as cols,
-    array_agg(_.udt_name::text) as types
+    array_agg(_.column_name::text order by ordinal_position) as cols,
+    array_agg(_.udt_name::text order by ordinal_position) as types,
+    array_agg(shift order by ordinal_position) as shifts,
+    null::int as curleft,
+    null::text as prev_column_name
   from
     combined_columns _
   group by is_orig, table_schema, table_name
@@ -68,33 +77,30 @@ with recursive constants as (
     table_schema,
     table_name,
     analyzed + 1,
-    case when cur_left_in_chunk <= 0 then chunk_size else cur_left_in_chunk end,
-    case when cur_left_in_chunk < 0 then padded_columns || array[prev_column_name] else padded_columns end,
-    case when cur_left_in_chunk < 0 then pads || array[remains_in_chunk] else pads end,
-    col_cnt,  
+    cur_left_in_chunk,
+    case when padding_occured > 0 then padded_columns || array[prev_column_name] else padded_columns end,
+    case when padding_occured > 0 then pads || array[padding_occured] else pads end,
+    col_cnt,
     cols,
-    types
+    types,
+    shifts,
+    cur_left_in_chunk,
+    ext.column_name as prev_column_name
   from analyze_alignment a, constants, lateral (
     select
-      left_in_chunk - coalesce(
-        (
-          select coalesce(shift, 0) /*todo*/
-          from combined_columns c  -- <<<<!!!!!!!
-          where ordinal_position = analyzed + 1 and c.is_orig = a.is_orig and c.table_name = a.table_name and c.table_schema = a.table_schema
-        ),
-        0
-      ) as cur_left_in_chunk,
-      (
-        select column_name::text
-        from combined_columns c
-        where ordinal_position = analyzed and c.is_orig = a.is_orig and c.table_name = a.table_name and c.table_schema = a.table_schema
-      ) as prev_column_name,
-      (
-        select (chunk_size - a.left_in_chunk)::int from constants
-      ) as remains_in_chunk
+      shift,
+      case when left_in_chunk < shift then left_in_chunk else 0 end as padding_occured,
+      case when left_in_chunk < shift then chunk_size - shift % chunk_size else left_in_chunk - shift end as cur_left_in_chunk,
+      column_name
+    from combined_columns c, constants
+    where
+      ordinal_position = a.analyzed + 1
+      and c.is_orig = a.is_orig
+      and c.table_name = a.table_name
+      and c.table_schema = a.          table_schema
   ) as ext
   where
-    analyzed <= col_cnt and analyzed < 1000/*sanity*/
+    analyzed < col_cnt and analyzed < 1000/*sanity*/
 ), result_pre as (
   select distinct on (is_orig, table_schema, table_name)
     is_orig ,
@@ -102,23 +108,21 @@ with recursive constants as (
     table_name,
     padded_columns,
     pads,
-    (select sum(p) from unnest(pads) _(p)) + (
-      select left_in_chunk
-      from analyze_alignment a2
-      where a2.table_schema = a1.table_schema and a2.table_name = a1.table_name
-      order by analyzed desc
-      limit 1
-    ) as padding_sum,
+    curleft,
+    coalesce((select sum(p) from unnest(pads) _(p)), 0) + (chunk_size + a1.curleft) % chunk_size as padding_sum,
     n_live_tup,
     n_dead_tup,
     c.oid as oid,
     pg_total_relation_size(c.oid) - pg_indexes_size(c.oid) - coalesce(pg_total_relation_size(reltoastrelid), 0) as table_bytes,
     cols,
-    types
+    types,
+    shifts,
+    analyzed
   from analyze_alignment a1
   join pg_namespace n on n.nspname = table_schema
   join pg_class c on n.oid = c.relnamespace and c.relname = table_name
   join pg_stat_user_tables s on s.schemaname = table_schema and s.relname = table_name
+  join constants on true
   order by is_orig, table_schema, table_name, analyzed desc
 ), result_both as (
   select
@@ -136,6 +140,20 @@ with recursive constants as (
     r1.padding_sum - coalesce(r2.padding_sum, 0) as padding_sum,
     r1.padding_sum as r1_padding_sum,
     r1.padding_total_est as r1_padding_total_est,
+    r2.padding_sum as r2_padding_sum,
+    r2.padding_total_est as r2_padding_total_est,
+    r1.cols,
+    r1.types,
+    r1.shifts,
+    r2.cols as alt_cols,
+    r2.types as alt_types,
+    r2.shifts as alt_shits,
+    r1.pads,
+    r1.curleft,
+    r2.pads as alt_pads,
+    r2.curleft as alt_curleft,
+    r1.padded_columns,
+    r1.analyzed,
     case
       when r1.table_bytes > 0 then
         round(100 * (r1.padding_sum - coalesce(r2.padding_sum, 0))::numeric * (r1.n_live_tup + r1.n_dead_tup)::numeric / r1.table_bytes, 2)
