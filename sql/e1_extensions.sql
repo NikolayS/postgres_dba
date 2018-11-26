b45551d78c3793b3ffa6f2ada321dc94d572841a
Nikolay <github2@samokhvalov.com>
2018-11-26 04:11:06 -0800

    Update e1_extensions.sql

--- sql/e1_extensions.sql
+++ sql/e1_extensions.sql
@@ -4,7 +4,7 @@ select
   ae.name,
   installed_version,
   default_version,
-  case when installed_version <> default_version then 'OLD' end as actuality
+  case when installed_version <> default_version then 'OLD' end as is_old
 from pg_extension e
 join pg_available_extensions ae on extname = ae.name
 order by ae.name;
