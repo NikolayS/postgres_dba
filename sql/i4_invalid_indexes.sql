70b896eca74d18a42fb42b51216941a223414b27
dmius <uddm@mail.ru>
2019-01-14 16:19:10 +0300

    Invalid indexes reworked

--- sql/i4_invalid_indexes.sql
+++ sql/i4_invalid_indexes.sql
@@ -6,20 +6,16 @@
 -- (except plpgsql), and doesn't create anything (like views or smth)
 -- -- so feel free to use it in your clouds (Heroku, AWS RDS, etc)
 
--- It also does't do anything except reading system catalogs and
--- printing NOTICEs, so you can easily run it on your
---  production *master* database.
 -- (Keep in mind, that on replicas, the whole picture of index usage
 -- is usually very different from master).
 
 select 
-  schemaname,
-  relname,
-  indexrelname,
-  idx_scan,
-  idx_tup_read,
-  idx_tup_fetch
+    coalesce(nullif(pn.nspname, 'public') || '.', '') || pct.relname as "relation_name",
+    pci.relname as index_name,
+    pn.nspname as schema_name,
+    pct.relname as table_name
 from pg_index pidx
-join pg_stat_user_indexes as idx_stat on idx_stat.indexrelid = pidx.indexrelid
+join pg_class as pci on pci.oid = pidx.indexrelid
+join pg_class as pct on pct.oid = pidx.indrelid
+left join pg_namespace pn on pn.oid = pct.relnamespace
 where pidx.indisvalid = false;
-
