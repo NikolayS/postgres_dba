0ab3bbb198925d56e8ecf9794925acca1da87963
Nikolay Samokhvalov <github2@samokhvalov.com>
2021-10-30 17:23:46 -0700

    Update i5_indexes_migration.sql

--- sql/i5_indexes_migration.sql
+++ sql/i5_indexes_migration.sql
@@ -1,4 +1,4 @@
---Unused/Redundant Indexes Do & Undo Migration DDL
+--Cleanup unused and redundant indexes – DO & UNDO migration DDL
 
 -- Use it to generate a database migration (e.g. RoR's db:migrate or Sqitch)
 -- to drop unused and redundant indexes.
